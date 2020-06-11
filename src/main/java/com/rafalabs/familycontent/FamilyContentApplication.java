408eb608692c0a4fff4d71a0e0f18608658c5e35
Rafael Vieira <rhpvieira@gmail.com>
2020-06-11 18:46:31 +0100

    Initial commit

---
+++ src/main/java/com/rafalabs/familycontent/FamilyContentApplication.java
@@ -0,0 +1,13 @@
+package com.rafalabs.familycontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyContentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafalabs/familycontent/FamilyContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalabs.familycontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

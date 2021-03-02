93db6cddac1c230f90f3eb6452b3074822a39c66
Rafael Vieira <rhpvieira@gmail.com>
2021-03-02 16:04:43 +0000

    Unecessary private restriction

--- src/main/kotlin/com/rafalabs/socialcm/content/domain/Content.kt
+++ src/main/kotlin/com/rafalabs/socialcm/content/domain/Content.kt
@@ -3,17 +3,17 @@ package com.rafalabs.socialcm.content.domain
 import java.lang.IllegalArgumentException
 
 class Content {
-    val title: String
-    val description: String
-    val value: String
+    val title: String;
+    val description: String;
+    val value: String;
 
     private constructor(title: String, description: String = "", value: String = "") {
         this.title =
             if(title.isBlank())
                 throw IllegalArgumentException("Title cannot be blank")
-            else title
-        this.description = description.trim()
-        this.value = value.trim()
+            else title;
+        this.description = description.trim();
+        this.value = value.trim();
     }
 
     data class Builder(val title: String) {

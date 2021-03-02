aae7e0c7660d71ddcb0a69402c5cb5ed4a7193a0
Rafael Vieira <rhpvieira@gmail.com>
2021-03-02 12:51:39 +0000

    Project restructuring

---
+++ src/test/kotlin/com/rafalabs/socialcm/SocialContentManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rafalabs.socialcm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialContentManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

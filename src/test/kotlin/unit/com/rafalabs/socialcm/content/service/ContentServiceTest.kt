f3779e2fd59d2b4b9652975386bfa0171f738acc
Rafael Vieira <rhpvieira@gmail.com>
2021-03-03 17:22:29 +0000

    Mocking other test

--- src/test/kotlin/unit/com/rafalabs/socialcm/content/service/ContentServiceTest.kt
+++ src/test/kotlin/unit/com/rafalabs/socialcm/content/service/ContentServiceTest.kt
@@ -6,14 +6,13 @@ import com.rafalabs.socialcm.content.repository.entity.ContentEntity
 import com.rafalabs.socialcm.content.service.ContentService
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.Mockito.*
+import org.mockito.Mockito.any
+import org.mockito.Mockito.doReturn
 import org.mockito.junit.jupiter.MockitoExtension
 
 
@@ -58,24 +57,39 @@ class ContentServiceTest {
 
     @Test
     internal fun should_CreateContent_When_Optional_Properties_Not_Filled() {
-        val content =
-                Content.Builder("Learning Kotlin by using TDD")
-                    .description(" ")
-                    .value(" ")
-                    .build();
+        // Given
+        val expectedContent =
+            Content.Builder("Learning Kotlin by using TDD")
+                .description("   ")
+                .value("   ")
+                .build();
 
-        assertEquals(content.description, "")
-        assertEquals(content.value, "")
+        val expectedContentEntity = ContentEntity();
+        expectedContentEntity.id            = 1;
+        expectedContentEntity.title         = expectedContent.title;
+        expectedContentEntity.description   = expectedContent.description;
+        expectedContentEntity.value         = expectedContent.value;
+
+        doReturn(expectedContentEntity)
+            .`when`(contentRepository)
+            .save(any());
+
+        // When
+        val persistedContent = contentService.save(expectedContent);
+
+        // Then
+        assertEquals(1,                     persistedContent.id);
+        assertEquals(expectedContent.title,         persistedContent.title);
+        assertEquals(expectedContent.description,   persistedContent.description);
+        assertEquals(expectedContent.value,         persistedContent.value);
     }
 
     @Test
     internal fun should_ThrowException_When_TitleIsBlank() {
 
         assertThrows(IllegalArgumentException::class.java) {
-            Content.Builder(" ")
-                    .description("How to learn Kotlin through TDD process")
-                    .value("Some big content")
-                    .build();
+            Content.Builder("   ")
+                .build();
         };
     }
 }

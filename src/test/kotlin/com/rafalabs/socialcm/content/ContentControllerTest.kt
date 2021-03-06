9f4ea30ca4d61ab9f855e5a5d5f31498a86db1c8
Rafael Vieira <rhpvieira@gmail.com>
2021-03-06 11:01:19 +0000

    Data script fixes + test for POST /api/content method

--- src/test/kotlin/com/rafalabs/socialcm/content/ContentControllerTest.kt
+++ src/test/kotlin/com/rafalabs/socialcm/content/ContentControllerTest.kt
@@ -7,10 +7,12 @@ import com.rafalabs.socialcm.content.repository.ContentRepository
 import com.rafalabs.socialcm.content.repository.entity.ContentEntity
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -28,6 +30,33 @@ class ContentControllerTest {
 
     @Autowired
     private lateinit var restClient: TestRestTemplate;
+
+    @Test
+    internal fun should_CreateContent_When_Properties_Filled() {
+        // Given
+        val expectedContent = ContentDTO();
+        expectedContent.title = "Learning Kotlin by using TDD";
+        expectedContent.description = "How to learn Kotlin through TDD process";
+        expectedContent.value = "Some big content";
+
+        // When
+        val response: ResponseEntity<Unit> =
+            restClient
+                .postForEntity(
+                    """${ContentController.BASE_URL}""",
+                    expectedContent
+                );
+
+        // Then
+        assertEquals(HttpStatus.CREATED, response.statusCode);
+//        assertEquals("Title 1", response.body?.title);
+//
+//        // Then
+//        assertEquals(1,                     persistedContent.id);
+//        assertEquals(expectedContent.title,         persistedContent.title);
+//        assertEquals(expectedContent.description,   persistedContent.description);
+//        assertEquals(expectedContent.value,         persistedContent.value);
+    }
 
     @Test
     internal fun should_FindContent_When_IdExists() {
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO content (id, title, description, value) VALUES
-  (1, 'Title 1', 'Desc 1', 'Content 1'),
-  (2, 'Title 2', 'Desc 2', 'Content 2');
+INSERT INTO content (title, description, value) VALUES
+  ('Title 1', 'Desc 1', 'Content 1'),
+  ('Title 2', 'Desc 2', 'Content 2');
